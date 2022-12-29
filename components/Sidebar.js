1ff66d9d74585889c954b6fed6bfa0bd555e6afa
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2022-12-29 22:59:29 +0800

    header&avatar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
+import { Avatar } from '@mui/material';
 
 const Sidebar = () => {
-  return <Container>Sidebar</Container>;
+  return (
+    <Container>
+      <Header>
+        <UserAvatar src="https://images.pexels.com/photos/210547/pexels-photo-210547.jpeg?autocompress&cs=tinysrgb&dpr=1&w=500" />
+      </Header>
+    </Container>
+  );
 };
 
 export default Sidebar;
@@ -11,4 +18,24 @@ const Container = styled.div`
   min-width: 320px;
   max-width: 450px;
   height: 100%;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+  width: 100%;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
 `;
