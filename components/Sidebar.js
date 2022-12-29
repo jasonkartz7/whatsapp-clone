7991eed400b4ec88247ddb8f120fb929347378b8
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2022-12-29 22:32:05 +0800

    initial version

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Sidebar = () => {
+  return <Container>Sidebar</Container>;
+};
+
+export default Sidebar;
+
+const Container = styled.div`
+  background-color: #ffffff;
+  min-width: 320px;
+  max-width: 450px;
+  height: 100%;
+`;
