2443882e93b43f1b58f01ef77890390a9196232f
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-01-03 18:14:09 +0800

    search bar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,6 +3,7 @@ import { Avatar, IconButton } from '@mui/material';
 import Image from 'next/image';
 import ChatIcon from '@mui/icons-material/Chat';
 import CustomMoreVertical from './CustomMoreVertical';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Sidebar = () => {
   return (
@@ -19,6 +20,12 @@ const Sidebar = () => {
           <CustomMoreVertical />
         </IconsGroup>
       </Header>
+      <SearchChat>
+        <SearchBar>
+          <SearchIcon />
+          <SeacrhInput />
+        </SearchBar>
+      </SearchChat>
     </Container>
   );
 };
@@ -37,8 +44,8 @@ const Header = styled.div`
   position: sticky;
   top: 0;
   background-color: white;
-  justify-content: center;
-  align-items: center;
+  /* justify-content: center; */
+  /* align-items: center; */
   padding: 15px;
   height: 80px;
   border-bottom: 1px solid whitesmoke;
@@ -53,3 +60,22 @@ const UserAvatar = styled(Avatar)`
 `;
 
 const IconsGroup = styled.div``;
+
+const SearchChat = styled.div`
+  background-color: #f6f6f6;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 20px;
+`;
+
+const SearchBar = styled.div`
+  display: flex;
+  padding: 5px;
+  border-radius: 10px;
+  border-bottom: 1px solid #ededed;
+  background: white;
+`;
+
+const SeacrhInput = styled.input`
+  width: 100%;
+  border: none;
+`;
