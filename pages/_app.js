45cb68a9a7ab63b74f7c83e5e374605be300f7a8
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-01-05 14:59:53 +0800

    auth provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
+import { AuthProvider } from '../Auth';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
-import Login from './login';
 
 function MyApp({ Component, pageProps }) {
-  return <Login />;
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthProvider>
+      {' '}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
   );
 }
 
