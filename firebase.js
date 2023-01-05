9ebbbe18fed80715d43e77b48b458712c1535873
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-01-05 14:32:46 +0800

    firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmNtcnoEaizZPUzB0U3KLgayqUXJSC-j8',
+  authDomain: 'whatsapp-2-ac273.firebaseapp.com',
+  projectId: 'whatsapp-2-ac273',
+  storageBucket: 'whatsapp-2-ac273.appspot.com',
+  messagingSenderId: '826855179265',
+  appId: '1:826855179265:web:9c39ff0848c17f65e8ac00',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
