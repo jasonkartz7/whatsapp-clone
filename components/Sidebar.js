fd3e50219b6a67fd299fffb2fe957e45e0d4e139
Jason Chua <jason04@SMRT.COM.SG>
2023-01-11 15:30:42 +0800

    bugfix

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -69,7 +69,7 @@ const Sidebar = () => {
   return (
     <Container>
       <Header>
-        <UserAvatar src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        <UserAvatar src={currentUser.photoURL} />
         <IconsGroup>
           <IconButton>
             <Image src="/story.svg" alt="" height={24} width={24} />
